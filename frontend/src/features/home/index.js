df6e6100a025c4f873f06ae7e162bcb2f2563539
Ahmed Hosni <ahmedmhosni90@Gmail.com>
2025-12-10 02:43:40 +0200

    fix: Remove non-existent HomeInternational and HomeNew exports from home index

--- frontend/src/features/home/index.js
+++ frontend/src/features/home/index.js
@@ -1,4 +1,2 @@
 // Home feature module exports
 export { default as Home } from './pages/Home';
-export { default as HomeInternational } from './pages/HomeInternational';
-export { default as HomeNew } from './pages/HomeNew';
